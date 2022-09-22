3649d8bb3eb4f41e0b41c04c93cbd0775f24c686
yvanvrela <varelaivan30@gmail.com>
2022-09-21 23:12:45 -0400

    Add settings

---
+++ src/app/v1/config/settings.py
@@ -0,0 +1,32 @@
+import os
+from pydantic import BaseSettings
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Settings(BaseSettings):
+    # Private var
+    _db_name: str = os.getenv('DB_NAME')
+    db_user: str = os.getenv('DB_USER')
+    db_pass: str = os.getenv('DB_PASS')
+    db_host: str = os.getenv('DB_HOST')
+    db_port: str = os.getenv('DB_PORT')
+
+    secret_key: str = os.getenv('SECRET_KEY')
+    token_expire: int = os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES')
+
+    @property
+    def db_name(self):
+        """Change the db name
+
+        This function change the database name, if running of test.
+
+        Returns:
+
+            str: str database name.
+        """
+        if os.getenv('RUN_ENV') == 'test':
+            return f'test_{self._db_name}'
+
+        return self._db_name
