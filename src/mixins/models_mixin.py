7b9c3d48bb2c72c4d63852caf44aad8d3aeefe29
yvanvrela <varelaivan30@gmail.com>
2022-09-27 15:12:11 -0400

    Add mixins

---
+++ src/mixins/models_mixin.py
@@ -0,0 +1,59 @@
+from pydantic import BaseModel, Field, EmailStr
+
+
+class IDMixin(BaseModel):
+    id: int = Field(
+        ...,
+        ge=1,
+        description='Unique ID of the document.',
+        example='1',
+    )
+
+
+class EmailMixin(BaseModel):
+    email: EmailStr = Field(...)
+
+
+class NameMixin(BaseModel):
+    name: str = Field(
+        ...,
+        min_length=1,
+        max_length=40,
+        description='Unique name of the document.',
+        example='Social',
+    )
+
+
+class UsernameMixin(BaseModel):
+    username: str = Field(
+        ...,
+        min_length=1,
+        max_length=40,
+        example='username123',
+    )
+
+
+class DescriptionMixin(BaseModel):
+    description: str | None = Field(
+        ...,
+        min_length=1,
+        max_length=240,
+        example='This is a description.',
+    )
+
+
+class IconTypeMixin(BaseModel):
+    icon_type: str = Field(
+        min_length=1,
+        max_length=260,
+        example='name-icon',
+    )
+
+
+class PasswordMixin(BaseModel):
+    password: str = Field(
+        ...,
+        min_length=12,
+        max_length=64,
+        example='Mypassword123.'
+    )
