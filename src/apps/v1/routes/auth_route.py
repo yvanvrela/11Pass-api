319a67e07afea62c40e683544eb82296ac8b2db2
yvanvrela <varelaivan30@gmail.com>
2022-10-04 10:30:44 -0300

    Add signup

--- src/apps/v1/routes/auth_route.py
+++ src/apps/v1/routes/auth_route.py
@@ -9,4 +9,27 @@ from src.infra.providers import password_provider
 
 router = APIRouter()
 
+@router.post(path='/signup',
+            status_code=status.HTTP_201_CREATED,
+            response_model=user_schema.UserBase,
+            summary='Create a new user',
+            )
+def signup_user(user: user_schema.UserLogin = Body(...), session: Session = Depends(get_db)):
+    # Verify user
+    user_reference = UserRepository(session).get_user_by_email(user.email)
+    print(user_reference)
 
+    if user_reference:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail='Email already registered.'
+        )
+
+    # Generate the user secret key
+    user.secret_key = security.secret_key_generator()
+
+    # Create new user
+    user.password = password_provider.hash_password(user.password)
+    user_db = UserRepository(session).create_user(user)
+
+    return user_db
