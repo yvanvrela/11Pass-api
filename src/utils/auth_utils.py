319147ffeb0573eb8b00a87846d2cb1e86efb65f
yvanvrela <varelaivan30@gmail.com>
2022-10-04 11:54:10 -0300

    Add authenticate user

--- src/utils/auth_utils.py
+++ src/utils/auth_utils.py
@@ -3,6 +3,7 @@ from fastapi.security import OAuth2PasswordBearer
 # Database
 from sqlalchemy.orm import Session
 from src.infra.database.config.database import get_db
+from src.infra.providers import password_provider
 # Repository
 from src.infra.database.repositories.user_repository import UserRepository
 # JWT
@@ -19,6 +20,28 @@ url_prefix = settings.api_prefix_router
 
 # Instance the login url and to validate token
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl=f'{url_prefix}/auth/login')
+
+
+def authenticate_user(user_email: str, password: str, session: Session = Depends(get_db)) -> UserModel | bool:
+    """Authenticate user
+
+    This is function recibed the username and password and verify to exists and the password is correct.
+
+    Args:
+
+        user_email (str): This is the user email.
+        password (str): This is the user password.
+
+    Returns:
+
+        UserModel | bool: UserModel is the user object or bool default is False.
+    """
+    user = UserRepository(session).get_user_by_email(user_email)
+    if not user:
+        return False
+    if not password_provider.verify_password(password, user.password):
+        return False
+    return user
 
 
 def get_current_user(token: str = Depends(oauth2_scheme), session: Session = Depends(get_db)) -> UserModel:
