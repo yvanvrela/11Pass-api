d89bd035c504fc34389bfc7fdf2c6e0e952d1a2b
yvanvrela <varelaivan30@gmail.com>
2022-10-28 13:04:06 -0300

    Update the password size

--- src/infra/database/models/account_model.py
+++ src/infra/database/models/account_model.py
@@ -10,7 +10,7 @@ class AccountModel(Base):
     name = Column(String(length=40), unique=True, index=True)
     username = Column(String(length=40), index=True)
     email = Column(String(length=50), index=True)
-    password = Column(String(length=64))
+    password = Column(String(length=260))
     description = Column(String(length=240))
     page_url = Column(String(length=260))
     icon_type = Column(String(length=260))
