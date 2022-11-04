8082323c1ebd5c9f70cadeb7af435f0d6883799b
yvanvrela <varelaivan30@gmail.com>
2022-11-04 20:49:50 -0300

    Add migration

---
+++ migrations/versions/0c3d51b557ba_create_columns.py
@@ -0,0 +1,24 @@
+"""Create columns
+
+Revision ID: 0c3d51b557ba
+Revises: 
+Create Date: 2022-11-04 23:37:04.995254
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '0c3d51b557ba'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
